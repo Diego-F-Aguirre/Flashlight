15edecfafc7ba7542dd6ef87078da9f9f0fc07ab
Diego-F-Aguirre <diego3884@gmail.com>
2016-02-01 21:01:53 -0700

    Flashlight complete
    fully functional flashlight

--- Flashlight/ViewController.swift
+++ Flashlight/ViewController.swift
@@ -10,6 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button: UIButton!
+    var isOn: Bool = false
+    
+    @IBAction func buttonTapped(sender: AnyObject) {
+        
+        
+        
+        if isOn == true {
+            self.button.setTitle("On", forState: UIControlState.Normal)
+            self.view.backgroundColor = UIColor.whiteColor()
+            self.button.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
+            isOn = false
+        } else {
+            self.view.backgroundColor = UIColor.blackColor()
+            self.button.setTitle("Off", forState: UIControlState.Normal)
+            self.button.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+            isOn = true
+            
+        }
+
+    }
+    
+    
+ 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
